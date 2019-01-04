733190f8fa6324b4e9107e9749ad651732083f73
revility <32818302+revility@users.noreply.github.com>
2019-01-04 13:32:09 -0500

    touch of death, and player immunity to damage
    player now takes damage when touching idactors with touchofdeath key set to 1.  immunity to damage script is called after taking damage.  Script is located in ai_player.script.  While script is active;  player takes no damage briefly and skin changes to flashing skin.  search rev 2018 for changes.

--- game/Player.cpp
+++ game/Player.cpp
@@ -26,7 +26,7 @@ If you have questions concerning this license or the applicable additional terms
 ===========================================================================
 */
 
-#include "sys/platform.h"
+#include "sys/platform.h" 
 #include "idlib/LangDict.h"
 #include "framework/async/NetworkSystem.h"
 #include "framework/DeclEntityDef.h"
